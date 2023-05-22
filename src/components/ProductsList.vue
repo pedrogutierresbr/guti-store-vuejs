ba3c53c096e2cfc99fea4167cce03d7a48bbd653
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-22 15:37:15 -0300

    created json with database and populated productlist with data

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,10 +1,34 @@
 <template>
-	<div>ProductsList</div>
+	<section class="products-container">
+		<ul>
+			<li v-for="product in products" :key="product.id">
+				<img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0]" />
+				<p class="price">{{ product.preco }}</p>
+				<h2 class="title">{{ product.nome }}</h2>
+				<p>{{ product.descricao }}</p>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <script>
 	export default {
 		name: "ProductsList",
+		data() {
+			return {
+				products: null,
+			};
+		},
+		methods: {
+			getProducts() {
+				fetch("http://localhost:3000/produto")
+					.then((response) => response.json())
+					.then((data) => (this.products = data));
+			},
+		},
+		created() {
+			this.getProducts();
+		},
 	};
 </script>
 
