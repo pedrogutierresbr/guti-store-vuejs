f79497cfd2569d18e966b53184ca03866e254a68
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-22 14:13:31 -0300

    added assets, created header and footer components and styled both

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,88 @@
 <template>
 	<div id="app">
-		<nav>
-			<router-link to="/">Home</router-link>
-		</nav>
-		<router-view />
+		<HeaderComponent />
+		<main id="main">
+			<router-view />
+		</main>
+		<FooterComponent />
 	</div>
 </template>
 
-<style></style>
+<script>
+	import HeaderComponent from "@/components/HeaderComponent.vue";
+	import FooterComponent from "@/components/FooterComponent.vue";
+
+	export default {
+		name: "App",
+		components: {
+			HeaderComponent,
+			FooterComponent,
+		},
+	};
+</script>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	body,
+	ul,
+	li,
+	h1,
+	h2,
+	p {
+		margin: 0;
+		padding: 0;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	body {
+		font-family: Arial, Helvetica, sans-serif;
+		color: #345;
+		background: url("./assets/pattern.svg") repeat top;
+	}
+
+	a {
+		color: #345;
+		text-decoration: none;
+	}
+
+	img {
+		max-width: 100%;
+		display: block;
+	}
+
+	#app {
+		display: flex;
+		min-height: 100vh;
+		flex-direction: column;
+	}
+
+	#main {
+		flex: 1;
+	}
+
+	.btn {
+		display: block;
+		padding: 10px 30px;
+		background: #87f;
+		border-radius: 4px;
+		color: #fff;
+		text-align: center;
+		font-size: 1rem;
+		box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+		transition: all 0.3s;
+		border: none;
+		font-family: Arial, Helvetica, sans-serif;
+		cursor: pointer;
+	}
+
+	.btn:hover {
+		background: #65d;
+		transform: scale(1.1);
+	}
+</style>
