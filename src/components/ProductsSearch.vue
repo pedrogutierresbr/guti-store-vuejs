fd2d5ebcaafc9913f53fefdb0c5c8a32c735952d
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-22 21:58:33 -0300

    styled productSearch

--- src/components/ProductsSearch.vue
+++ src/components/ProductsSearch.vue
@@ -1,6 +1,6 @@
 <template>
 	<form>
-		<input type="text" name="searchInput" id="searchInput" v-model="search" />
+		<input type="text" name="searchInput" id="searchInput" v-model="search" placeholder="Buscar..." />
 		<input type="submit" name="searchBtn" id="searchBtn" value="Buscar" @click.prevent="searchProducts" />
 	</form>
 </template>
@@ -21,4 +21,34 @@
 	};
 </script>
 
-<style></style>
+<style scoped>
+	form {
+		max-width: 600px;
+		margin: 30px auto 60px auto;
+		position: relative;
+	}
+
+	#searchInput {
+		width: 100%;
+		padding: 20px;
+		border: none;
+	}
+
+	#searchInput:focus,
+	#searchInput:hover {
+		transform: scale(1.1);
+	}
+
+	#searchBtn {
+		width: 62px;
+		height: 62px;
+		background: url("../assets/search.svg") no-repeat center center;
+		text-indent: -150px;
+		border: none;
+		cursor: pointer;
+		position: absolute;
+		top: 0;
+		right: 0;
+		box-shadow: none;
+	}
+</style>
