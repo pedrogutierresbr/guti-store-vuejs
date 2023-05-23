cb6fe713e49507934049e79176bfd4e262843c98
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-22 21:43:10 -0300

    stylized products

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,13 +1,19 @@
 <template>
 	<section class="products-container">
-		<ul>
-			<li v-for="product in products" :key="product.id">
-				<img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0]" />
-				<p class="price">{{ product.preco }}</p>
-				<h2 class="title">{{ product.nome }}</h2>
-				<p>{{ product.descricao }}</p>
-			</li>
-		</ul>
+		<div v-if="products && products.length > 0" class="products">
+			<div v-for="product in products" :key="product.id" class="product">
+				<router-link to="/">
+					<img v-if="product.fotos" :src="product.fotos[0].src" :alt="product.fotos[0]" />
+					<p class="price">{{ product.preco }}</p>
+					<h2 class="title">{{ product.nome }}</h2>
+					<p>{{ product.descricao }}</p>
+				</router-link>
+			</div>
+		</div>
+		<div v-else-if="products && products.length === 0" class="no-results">
+			<p>Busca sem resultados. Tente outro termo.</p>
+		</div>
+		<div v-else></div>
 	</section>
 </template>
 
@@ -19,7 +25,7 @@
 		name: "ProductsList",
 		data() {
 			return {
-				products: null,
+				products: [],
 				productsPerPage: 9,
 			};
 		},
@@ -45,4 +51,49 @@
 	};
 </script>
 
-<style></style>
+<style scoped>
+	.products-container {
+		max-width: 1000px;
+		margin: 0 auto;
+	}
+
+	.products {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		gap: 30px;
+		margin: 30px;
+	}
+
+	.product {
+		box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+		padding: 10px;
+		background: #fff;
+		border-radius: 4px;
+		transition: all 0.2s;
+	}
+
+	.product:hover {
+		box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+		transform: scale(1.1);
+		position: relative;
+		z-index: 1;
+	}
+
+	.product img {
+		border-radius: 4px;
+		margin-bottom: 20px;
+	}
+
+	.title {
+		margin-bottom: 10px;
+	}
+
+	.price {
+		color: #e80;
+		font-weight: bold;
+	}
+
+	.no-results {
+		text-align: center;
+	}
+</style>
