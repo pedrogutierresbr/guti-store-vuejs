6868e2c62450380d8a5b19769a1ca99bd43c07b2
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2023-05-26 11:11:53 -0300

    created LoginCreate and UserForm components

--- src/components/Login/LoginCreate.vue
+++ src/components/Login/LoginCreate.vue
@@ -1,11 +1,45 @@
 <template>
-	<p>Criar Login</p>
+	<section>
+		<h2>Criar a sua conta</h2>
+		<transition mode="out-in">
+			<button v-if="!create" class="btn" @click="create = true">Criar Conta</button>
+			<UserForm v-else>
+				<button class="btn btn-form">Criar Usuário</button>
+			</UserForm>
+		</transition>
+	</section>
 </template>
 
 <script>
+	import UserForm from "@/components/UserComponents/UserForm.vue";
+
 	export default {
 		name: "LoginCreate",
+		components: {
+			UserForm,
+		},
+		data() {
+			return {
+				create: false,
+			};
+		},
 	};
 </script>
 
-<style></style>
+<style scoped>
+	h2 {
+		text-align: center;
+		margin-top: 40px;
+		margin-bottom: 10px;
+	}
+
+	.btn {
+		width: 100%;
+		max-width: 300px;
+		margin: 0 auto;
+	}
+
+	.btn-form {
+		max-width: 100%;
+	}
+</style>
